0c9e9f427fa0a4f1a5fc515f93aa36e04469271d
choch-o <hscho1226@gmail.com>
2017-01-20 20:01:45 +0900

    Checks login status on MainActivity and starts LoginActivity if user is not logged in

--- app/Loadmap/app/src/main/java/com/loadmap/chocho/loadmap/CourseListActivity.java
+++ app/Loadmap/app/src/main/java/com/loadmap/chocho/loadmap/CourseListActivity.java
@@ -3,6 +3,8 @@ package com.loadmap.chocho.loadmap;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 /**
@@ -13,6 +15,9 @@ public class CourseListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_course_list);
 
         TextView statusView = (TextView) findViewById(R.id.status);
