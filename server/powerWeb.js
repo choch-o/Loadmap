cff894b2074d554b21418d6d2a2aa204482dcad9
choch-o <hscho1226@gmail.com>
2017-01-21 19:41:53 +0000

    POST courses data

--- server/powerWeb.js
+++ server/powerWeb.js
@@ -7,56 +7,59 @@ var app = new express;
 app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loadmap');
+
 var Schema = mongoose.Schema;
 
 var taskDataSchema = new Schema({
-	randomkey : {type:Number, required : true},
-	subject : {type:String, required : true},
-	tasktype : {type:String, required : true},
-	date : {type:String, required : true},
-	time : {type:String, required : true},
-	taskstatus : {type:Number, default : 0}
+  randomkey : {type:Number, required : true},
+  subject : {type:String, required : true},
+  tasktype : {type:String, required : true},
+  date : {type:String, required : true},
+  time : {type:String, required : true},
+  taskstatus : {type:Number, default : 0}
 });
 var Task = mongoose.model('task', taskDataSchema, 'task');
 
+var api = require('./api');
 
-
+app.post('/courses', api.saveCourses);
 app.post('/task/data', function(req,res){
-	console.log("[task/data] Got request");
+  console.log("[task/data] Got request");
 
-	Task.find({randomkey:req.body['randomkey']}, function(err, result){
-		console.log("          "+result)
-		if(result.length>0){
-		var newTask={};
+  Task.find({randomkey:req.body['randomkey']}, function(err, result){
+    console.log("          "+result)
+    if(result.length>0){
+      var newTask={};
 
-		if(req.body['subject']) newTask.subject=req.body['subject'];
-		if(req.body['tasktype']) newTask.tasktype=req.body['tasktype'];
-		if(req.body['date']) newTask.date=req.body['date'];
-		if(req.body['time']) newTask.subject=req.body['time'];
-		if(req.body['taskstatus']) newTask.taskstatus=req.body['taskstatus'];
+      if(req.body['subject']) newTask.subject=req.body['subject'];
+      if(req.body['tasktype']) newTask.tasktype=req.body['tasktype'];
+      if(req.body['date']) newTask.date=req.body['date'];
+      if(req.body['time']) newTask.subject=req.body['time'];
+      if(req.body['taskstatus']) newTask.taskstatus=req.body['taskstatus'];
 
-		Task.findOneAndUpdate({randomkey:req.body['randomkey']}, newTask, {}, function(err,results){
-			if (err) throw err;
-			console.log("DONE UPDATE TASK " + results);
-		});
-	} else {
-		var newTask = {
-			randomkey : req.body['randomkey'],
-			tasktype : req.body['tasktype'],
-			date : req.body['date'],
-			time : req.body['time'],
-			taskstatus : req.body['taskstatus']
-		};
+      Task.findOneAndUpdate({randomkey:req.body['randomkey']}, newTask, {}, function(err,results){
+        if (err) throw err;
+        console.log("DONE UPDATE TASK " + results);
+      });
+    } else {
+      var newTask = {
+        randomkey : req.body['randomkey'],
+        tasktype : req.body['tasktype'],
+        date : req.body['date'],
+        time : req.body['time'],
+        taskstatus : req.body['taskstatus']
+      };
 
-		User.findOneAndUpdate({randomkey : req.body['randomkey']}, newTask, {upsert: true, new: true}, function (err, results){
-		if(err) throw err;
-		console.log("DONE ENROLL NEW TASK " + results);
-		});
-	}
-});
-	res.writeHead(200, {'Content-Type':'application/json'});
-//	res.write(JSON,stringify({result: 'OK'}));
-	res.end();
+      User.findOneAndUpdate({randomkey : req.body['randomkey']}, newTask, {upsert: true, new: true}, function (err, results){
+        if(err) throw err;
+        console.log("DONE ENROLL NEW TASK " + results);
+      });
+    }
+  });
+  res.writeHead(200, {'Content-Type':'application/json'});
+  //	res.write(JSON,stringify({result: 'OK'}));
+  res.end();
 });
 
 app.listen (3000, function() {console.log("Listening on port #3000")});
