a291f92a279ae933f1c0678c1b7411c37454a848
SANHA PARK <psanha@kaist.ac.kr>
2017-01-22 12:44:45 +0000

    subject io

--- server/powerWeb.js
+++ server/powerWeb.js
@@ -20,6 +20,16 @@ var taskDataSchema = new Schema({
 var Task = mongoose.model('task', taskDataSchema, 'task');
 
 
+app.get('/task/viewdata', function(req,res){
+	Task.find({}, function(err,results){
+		if(err) throw err;
+
+		res.writeHead(200, {'Content-Type':'application/json'});
+		res.write(JSON.stringify(results));
+		res.end();
+	});
+});
+
 
 app.post('/task/data', function(req,res){
 	console.log("[task/data] Got request");
@@ -55,7 +65,7 @@ app.post('/task/data', function(req,res){
 	}
 });
 	res.writeHead(200, {'Content-Type':'application/json'});
-//	res.write(JSON,stringify({result: 'OK'}));
+	res.write(JSON,stringify({result: 'OK'}));
 	res.end();
 });
 
