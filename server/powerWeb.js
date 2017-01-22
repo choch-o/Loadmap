81f4a294abfb89646ca4155bda9b68ca0b029ee8
SANHA PARK <psanha@kaist.ac.kr>
2017-01-22 13:01:31 +0000

    fucking conflict

--- server/powerWeb.js
+++ server/powerWeb.js
@@ -23,7 +23,6 @@ var Task = mongoose.model('task', taskDataSchema, 'task');
 
 var api = require('./api');
 
-<<<<<<< HEAD
 app.get('/task/viewdata', function(req,res){
 	Task.find({}, function(err,results){
 		if(err) throw err;
@@ -34,44 +33,6 @@ app.get('/task/viewdata', function(req,res){
 	});
 });
 
-
-app.post('/task/data', function(req,res){
-	console.log("[task/data] Got request");
-
-	Task.find({randomkey:req.body['randomkey']}, function(err, result){
-		console.log("          "+result)
-		if(result.length>0){
-		var newTask={};
-
-		if(req.body['subject']) newTask.subject=req.body['subject'];
-		if(req.body['tasktype']) newTask.tasktype=req.body['tasktype'];
-		if(req.body['date']) newTask.date=req.body['date'];
-		if(req.body['time']) newTask.subject=req.body['time'];
-		if(req.body['taskstatus']) newTask.taskstatus=req.body['taskstatus'];
-
-		Task.findOneAndUpdate({randomkey:req.body['randomkey']}, newTask, {}, function(err,results){
-			if (err) throw err;
-			console.log("DONE UPDATE TASK " + results);
-		});
-	} else {
-		var newTask = {
-			randomkey : req.body['randomkey'],
-			tasktype : req.body['tasktype'],
-			date : req.body['date'],
-			time : req.body['time'],
-			taskstatus : req.body['taskstatus']
-		};
-
-		User.findOneAndUpdate({randomkey : req.body['randomkey']}, newTask, {upsert: true, new: true}, function (err, results){
-		if(err) throw err;
-		console.log("DONE ENROLL NEW TASK " + results);
-		});
-	}
-});
-	res.writeHead(200, {'Content-Type':'application/json'});
-	res.write(JSON,stringify({result: 'OK'}));
-	res.end();
-=======
 // app.get('/courses', api.getCourses);
 app.get('/courses/:user_id', api.getCourses);
 app.post('/courses', api.saveCourses);
@@ -111,7 +72,6 @@ app.post('/task/data', function(req,res){
   res.writeHead(200, {'Content-Type':'application/json'});
   //	res.write(JSON,stringify({result: 'OK'}));
   res.end();
->>>>>>> aaff14da6c6dfa640f66ff3216a95d9f392b6a3f
 });
 
 app.listen (3000, function() {console.log("Listening on port #3000")});
