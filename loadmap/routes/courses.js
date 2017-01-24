c29482db35f0ada5ac0e19f69c1fd12bc3fde9b0
choch-o <hscho1226@gmail.com>
2017-01-24 15:35:05 +0000

    web basis + search bar + POST request + saves Courses to db

--- loadmap/routes/courses.js
+++ loadmap/routes/courses.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var async = require('async');
 var mongoose = require('mongoose');
 
 var Course = require('../models/Course');
@@ -15,6 +16,7 @@ router.post('/', function(req, res) {
       console.log(user);
       var newUser = new User(req.body);
       newUser.save();
+
       res.send(user);
     } else {
       user.courses = req.body.courses || user.courses;
@@ -26,6 +28,41 @@ router.post('/', function(req, res) {
         res.send(user);
       });
     }
+  });
+
+  var calls = [];
+
+  var i;
+  req.body.courses.forEach(function (course) {
+    calls.push(function(callback) {
+      Course.find({
+        name: course.name,
+        semester: course.semester
+      }, function(error, result) {
+        console.log(course);
+        console.log("@@@@")
+        console.log(result);
+        console.log("##$");
+        if (result.length == 0) {
+          console.log("NULL");
+          var newCourse = new Course({
+            name: course.name,
+            professor: course.professor,
+            code: course.code,
+            semester: course.semester
+          });
+          newCourse.save();
+        } else {
+          console.log("NOT NULL");
+        }
+      });
+    });
+  });
+
+  async.parallel(calls, function(err, result) {
+    if (err)
+      return console.log(err);
+    console.log(result);
   });
 });
 
@@ -39,5 +76,6 @@ router.get('/:user_id', function(req, res) {
     }
   });
 });
+
 
 module.exports = router;
